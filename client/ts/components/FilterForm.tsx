651fc496fd7dcbb320ee67f797ec084f1f38fed5
PhilipL <philipl@tw.ibm.com>
2019-03-25 01:10:11 +0800

    Add filter story

---
+++ client/ts/components/FilterForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FilterForm = () => (
+  <div>
+    I am Form.
+  </div>
+);
+
+export default FilterForm;
--- client/ts/stories/Component.stories.tsx
+++ client/ts/stories/Component.stories.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Button } from 'semantic-ui-react';
 
+import FilterForm from '../components/FilterForm';
+
 const stories = storiesOf('Component', module);
 
-stories.add(
+stories
+.add(
   'Normal Button',
   () => (
     <Button>Normal</Button>
+  ))
+.add(
+  'Form', () => (
+    <FilterForm />
   ));
