364226219eac3cd5a2e4508818f303117f395f87
PhilipL <philipl@tw.ibm.com>
2019-03-25 01:09:28 +0800

    Add storybook config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,12 @@
+const {
+  clientConfig
+} = require('../webpack.config');
+
+module.exports = ({
+  config,
+  mode
+}) => {
+  config.module = clientConfig.module;
+  config.resolve = clientConfig.resolve;
+  return config;
+};
