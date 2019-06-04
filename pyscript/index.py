395cde8f3aa889ac7c03c210e04f10065aa0da5a
Philip Lin <honesty1997@gmail.com>
2019-06-05 00:38:29 +0800

    Fix python scripts error on windows

--- pyscript/index.py
+++ pyscript/index.py
@@ -2,18 +2,23 @@ import os
 from worker import worker
 from multiprocessing.pool import Pool
 
-worker_number = os.cpu_count()
-worker_pool = Pool(worker_number)
 
-for _ in range(worker_number):
-    worker_pool.apply_async(worker, [])
+def main():
+    worker_number = os.cpu_count()
+    worker_pool = Pool(worker_number)
 
-worker_pool.close()
+    for _ in range(worker_number):
+        worker_pool.apply_async(worker, [])
 
-try:
-    while True:
-        continue
-except KeyboardInterrupt:
-    print(' [*] Exiting')
-    worker_pool.terminate()
-    worker_pool.join()
+    worker_pool.close()
+
+    try:
+        while True:
+            continue
+    except KeyboardInterrupt:
+        print(' [*] Exiting')
+        worker_pool.terminate()
+        worker_pool.join() 
+
+if __name__ == '__main__':
+    main()
