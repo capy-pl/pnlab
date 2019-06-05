d3b833f17db719b5a845a8619b73d748bf34a084
Philip Lin <honesty1997@gmail.com>
2019-06-05 16:27:29 +0800

    Add add report api.

--- server/api/report/route.ts
+++ server/api/report/route.ts
@@ -3,12 +3,18 @@ import { loginRequired } from '../../core/auth';
 
 import {
   SearchItem,
-  GetConditions
+  GetConditions,
+  AddReport
 } from './controller';
 
 const router = Router();
 
 router.get('/searchItem', SearchItem);
 router.get('/conditions', loginRequired, GetConditions);
+
+router
+  .route('/')
+  .all(loginRequired)
+  .post(AddReport);
 
 export default router;
