f0626fe0c65ee3b5cc7d1e18b54840458a4d4f46
Tiffany Chen <loveseo1124@gmail.com>
2019-08-23 12:25:23 +0800

    refactor tabPanel

--- client/ts/pages/Analysis/Compare/CompareReportWindow/index.tsx
+++ client/ts/pages/Analysis/Compare/CompareReportWindow/index.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Grid, Icon, Tab, Table } from 'semantic-ui-react';
+import { Grid, Icon, Menu, MenuItemProps, Segment, Table } from 'semantic-ui-react';
 import { Window } from 'Component/';
 import AnalysisAPI from '../../../../PnApp/model/Analysis';
 import Report, { Condition, Node } from '../../../../PnApp/model/Report';
@@ -19,10 +19,26 @@ interface CompareReportWindowProps {
   conditions?: Condition[];
 }
 
-export default class CompareReportWindow extends PureComponent<
-  CompareReportWindowProps,
-  {}
-  > {
+interface CompareReportWindowState {
+  activeIndex: number;
+}
+export default class CompareReportWindow extends PureComponent<CompareReportWindowProps, CompareReportWindowState> {
+  constructor(props: CompareReportWindowProps) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+    };
+
+    this.onClickMenu = this.onClickMenu.bind(this);
+  }
+
+  public onClickMenu(e, props: MenuItemProps) {
+    const { index } = props;
+    this.setState({
+      activeIndex: index as number,
+    });
+  }
+
   public getTableRow(nodes: Node[], leftHandSideNodes: Node[] = []) {
     const tableRow = nodes.map((node, index) => {
       let style;
@@ -37,33 +53,29 @@ export default class CompareReportWindow extends PureComponent<
         if (!(leftNodeNames.indexOf(node.name) < 0)) {
           variation = leftNodeNames.indexOf(node.name) - nodeNames.indexOf(node.name);
         }
-        switch (true) {
-          case variation < 0:
-            arrow = (
-              <React.Fragment>
-                <Icon color='red' name='long arrow alternate down' />
-                <span style={{ color: 'red' }}>{Math.abs(variation)}</span>
-              </React.Fragment>
-            );
-            break;
-          case variation > 0:
-            arrow = (
-              <React.Fragment>
-                <Icon color='green' name='long arrow alternate up' />
-                <span style={{ color: 'green' }}>{Math.abs(variation)}</span>
-              </React.Fragment>
-            );
-            break;
-          case variation === 0:
-            arrow = (
-              <React.Fragment>
-                <Icon color='blue' name='minus' />
-                <span style={{ color: 'blue' }} />
-              </React.Fragment>
-            );
-            break;
-          default:
-            arrow = <React.Fragment />;
+        if (variation < 0) {
+          arrow = (
+            <React.Fragment>
+              <Icon color='red' name='long arrow alternate down' />
+              <span style={{ color: 'red' }}>{Math.abs(variation)}</span>
+            </React.Fragment>
+          );
+        } else if (variation > 0) {
+          arrow = (
+            <React.Fragment>
+              <Icon color='green' name='long arrow alternate up' />
+              <span style={{ color: 'green' }}>{Math.abs(variation)}</span>
+            </React.Fragment>
+          );
+        } else if (variation === 0) {
+          arrow = (
+            <React.Fragment>
+              <Icon color='blue' name='minus' />
+              <span style={{ color: 'blue' }} />
+            </React.Fragment>
+          );
+        } else {
+          arrow = <React.Fragment />;
         }
       }
       return (
@@ -84,19 +96,46 @@ export default class CompareReportWindow extends PureComponent<
     const leftHandSideNodes = leftHandSideReport ? leftHandSideReport.nodes : [];
     const tableRow = this.getTableRow(nodes, leftHandSideNodes);
     return (
-      <Table celled padded color='teal'>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>名次</Table.HeaderCell>
-            <Table.HeaderCell>圖{tableName}產品</Table.HeaderCell>
-            <Table.HeaderCell>權重</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+      <React.Fragment>
+        <p>圖{tableName}產品數： {report.nodes.length}</p>
+        <Table celled padded color='teal'>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>名次</Table.HeaderCell>
+              <Table.HeaderCell>圖{tableName}產品</Table.HeaderCell>
+              <Table.HeaderCell>權重</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-        <Table.Body>{tableRow}</Table.Body>
-      </Table>
+          <Table.Body>{tableRow}</Table.Body>
+        </Table>
+      </React.Fragment>
     );
   };
+
+  public getShareProductsTable() {
+    const share = this.props.shareNodes.map((node) => {
+      return (
+        <Table.Row key={node} style={{ backgroundColor: '#e8f7ff' }}>
+          <Table.Cell>{node}</Table.Cell>
+        </Table.Row>
+      );
+    });
+    return (
+      <React.Fragment>
+        <p>共同產品數： {this.props.shareNodes.length}</p>
+        <Table celled padded color='yellow'>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>共同產品</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>{share}</Table.Body>
+        </Table>
+      </React.Fragment>
+    );
+  }
 
   public getConditionRow(currentCondition: Condition, reportConditions: Condition[]) {
     let conditionRow;
@@ -114,19 +153,8 @@ export default class CompareReportWindow extends PureComponent<
     return conditionRow;
   }
 
-  public TabPanel = () => {
-    const reportA = this.props.reportA;
-    const reportB = this.props.reportB;
-    const tableA = this.getTable('左', reportA);
-    const tableB = this.getTable('右', reportB, this.props.reportA);
-    const share = this.props.shareNodes.map((node) => {
-      return (
-        <Table.Row key={node} style={{ backgroundColor: '#e8f7ff' }}>
-          <Table.Cell>{node}</Table.Cell>
-        </Table.Row>
-      );
-    });
-    const conditionRows = this.props.conditions.map((condition) => {
+  public getConditionTableBody() {
+    const conditionTableBody = this.props.conditions.map((condition) => {
       const conditionA = this.getConditionRow(condition, this.props.reportA.conditions);
       const conditionB = this.getConditionRow(condition, this.props.reportB.conditions);
       return (
@@ -137,81 +165,17 @@ export default class CompareReportWindow extends PureComponent<
         </Table.Row>
       );
     });
-
-    const panes = [
-      {
-        menuItem: '基本資料比較',
-        render: () => {
-          return (
-            <Tab.Pane>
-              <Grid>
-                <Grid.Row>
-                  <Grid.Column>
-                    <Table celled padded color='teal'>
-                      <Table.Header>
-                        <Table.Row>
-                          <Table.HeaderCell width={4}>條件名稱</Table.HeaderCell>
-                          <Table.HeaderCell>
-                            圖【{this.props.analysisA.title}】
-                          </Table.HeaderCell>
-                          <Table.HeaderCell>
-                            圖【{this.props.analysisB.title}】
-                          </Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
-
-                      <Table.Body>{conditionRows}</Table.Body>
-                    </Table>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Tab.Pane>
-          );
-        },
-      },
-      {
-        menuItem: '產品列表',
-        render: () => {
-          return (
-            <Tab.Pane>
-              <Grid>
-                <Grid.Row>
-                  <Grid.Column width={6}>
-                    <p>圖左產品數： {this.props.reportA.nodes.length}</p>
-                    {tableA}
-                  </Grid.Column>
-
-                  <Grid.Column width={4}>
-                    <p>共同產品數： {this.props.shareNodes.length}</p>
-                    <Table celled padded color='yellow'>
-                      <Table.Header>
-                        <Table.Row>
-                          <Table.HeaderCell>共同產品</Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
-
-                      <Table.Body>{share}</Table.Body>
-                    </Table>
-                  </Grid.Column>
-
-                  <Grid.Column width={6}>
-                    <p>圖右產品數： {this.props.reportB.nodes.length}</p>
-                    {tableB}
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Tab.Pane>
-          );
-        },
-      },
-    ];
-    return <Tab panes={panes} />;
-  };
+    return conditionTableBody;
+  }
 
   public render() {
     if (!this.props.show) {
       return <React.Fragment />;
     } else {
+      const conditionTableBody = this.getConditionTableBody();
+      const productTableA = this.getTable('左', this.props.reportA);
+      const productTableB = this.getTable('右', this.props.reportB, this.props.reportA);
+      const shareProductsTable = this.getShareProductsTable();
       return (
         <Window
           title='網路圖比較'
@@ -220,7 +184,51 @@ export default class CompareReportWindow extends PureComponent<
           defaultHeight={600}
           defaultWidth={1200}
         >
-          <this.TabPanel />
+          <React.Fragment>
+            <Menu attached='top' tabular>
+              <Menu.Item
+                onClick={this.onClickMenu}
+                index={0}
+                active={this.state.activeIndex === 0}
+              >
+                基本資料比較
+              </Menu.Item>
+              <Menu.Item
+                onClick={this.onClickMenu}
+                index={1}
+                active={this.state.activeIndex === 1}
+              >
+                產品列表
+              </Menu.Item>
+            </Menu>
+            <Segment hidden={this.state.activeIndex !== 0} attached='bottom'>
+              <Table celled padded color='teal'>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell width={4}>條件名稱</Table.HeaderCell>
+                    <Table.HeaderCell>圖【{this.props.analysisA.title}】</Table.HeaderCell>
+                    <Table.HeaderCell>圖【{this.props.analysisB.title}】</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>{conditionTableBody}</Table.Body>
+              </Table>
+            </Segment>
+            <Segment hidden={this.state.activeIndex !== 1} attached='bottom'>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={6}>
+                    {productTableA}
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    {shareProductsTable}
+                  </Grid.Column>
+                  <Grid.Column width={6}>
+                    {productTableB}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </React.Fragment>
         </Window>
       );
     }
