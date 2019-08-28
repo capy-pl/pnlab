88b488f288617236b0229e605d660d43bd3f4cb3
PhilipL <philipl@tw.ibm.com>
2019-08-28 10:42:56 +0800

    temp for import_data

--- pyscript/src/task/import_data.py
+++ pyscript/src/task/import_data.py
@@ -1,2 +1,12 @@
+from os import getenv
+from bson.objectid import ObjectId
+import traceback
+from datetime import datetime
+import logging
+
+from ..mongo_client import db
+
+
 def import_data(data_id):
-    pass
+    import_history = db['importHistories'].find_one({'_id': ObjectId(data_id)})
+    file_path = import_history['filepath'] + import_history['filename']
