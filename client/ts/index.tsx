c88e52ba1e0f29edcff8a7bd99f1f8b8f5d7c1c9
PhilipL <philipl@tw.ibm.com>
2019-11-22 14:18:58 +0800

    Change semantic UI to Fomantic UI.

--- client/ts/index.tsx
+++ client/ts/index.tsx
@@ -1,7 +1,7 @@
 // tslint:disable-next-line: no-reference
 /// <reference path="./index.d.ts" /> #
 // import css
-import 'semantic-ui-css/semantic.min.css';
+import 'fomantic-ui/dist/semantic.min.css';
 import 'vis-network/dist/vis-network.min.css';
 import '../scss/main';
 import '../scss/semanticui.override.scss';
